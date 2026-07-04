7821d563f85c467312408d5a92963c0d17b80c44
Hoshino Masafumi <53420696+pfboy77@users.noreply.github.com>
2026-07-05 04:13:37 +0900

    Simplify app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import PlayerView from "./PlayerView";
-import MonitorView from "./MonitorView";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/play" element={<PlayerView />} />
-        <Route path="/monitor" element={<MonitorView />} />
-      </Routes>
-    </Router>
-  );
+  return <div style={{ padding: 16 }}>Resource Companion</div>;
 }
 
 export default App;
